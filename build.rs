255b68fbf33be22ece39a1f82ac9801cafddefcd
pvlov <Daniel.Pavlov@gmx.de>
2024-07-09 09:49:00 +0200

    fix pipeline - remove unused import

--- build.rs
+++ build.rs
@@ -1,8 +1,6 @@
 use core::panic;
 use std::{env, error::Error, fs, process::Command};
 
-use dotenv::{dotenv, from_filename};
-
 fn generate_openapi() -> Result<(), Box<dyn Error>> {
     let input_dir = env::var("OPENAPI_INPUT_DIR")?;
     let output_dir = env::var("OPENAPI_OUTPUT_DIR")?;
@@ -39,6 +37,6 @@ fn generate_openapi() -> Result<(), Box<dyn Error>> {
 }
 
 fn main() {
-    dotenv().unwrap_or(dotenv::from_filename("template.env").expect("No .env or template.env present"));
+    dotenv::dotenv().unwrap_or(dotenv::from_filename("template.env").expect("No .env or template.env present"));
     generate_openapi().expect("Failed to Generate OpenAPI models");
 }
